21a496116e610f79c927eb8623a13597e3071d94
saasifyapp <saasifyapp@gmail.com>
2024-06-10 01:01:05 +0530

    added missing packages

--- App/main_server.js
+++ App/main_server.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 const app = express();
-
+ 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
