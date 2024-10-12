2daa4e125a0a37498f41a831a4b03d1e6e4ae51b
Yash Ingale <yashingale6@gmail.com>
2024-10-12 18:55:38 +0530

    build test app

--- testAndroidApp/www/js/index.js
+++ testAndroidApp/www/js/index.js
@@ -1,29 +1,24 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+document.addEventListener('deviceready', function() {
+    console.log('Device is ready');
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+    var latitudeElement = document.getElementById('latitude');
+    var longitudeElement = document.getElementById('longitude');
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+    function updateLocation() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+            latitudeElement.textContent = latitude;
+            longitudeElement.textContent = longitude;
+        }, function(error) {
+            console.error('Error getting location: ' + error.message);
+        });
+    }
+
+    // Update location every 2 seconds
+    setInterval(updateLocation, 2000);
+
+    // Initial location update
+    updateLocation();
+}, false);
