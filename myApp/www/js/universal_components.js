2f9718375ea4b950fe9031fd0220c327ae832d92
Yash Ingale <yashingale6@gmail.com>
2024-10-15 23:27:45 +0530

    fix 11

--- myApp/www/js/universal_components.js
+++ myApp/www/js/universal_components.js
@@ -1,31 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('./html/universal_components.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('universal-container').innerHTML = data;
-        })
-        .then(() => {
-            // Custom back button logic
-            const backButton = document.getElementById('back-button');
-            const backToConsoleButton = document.getElementById('back-to-console-button');
+    // Custom back button logic
+    const backButton = document.getElementById('back-button');
+    const backToConsoleButton = document.getElementById('back-to-console-button');
 
-            if (backButton) {
-                backButton.addEventListener('click', () => {
-                    window.location.href = './index.html';
-                });
-            }
-
-            if (backToConsoleButton) {
-                backToConsoleButton.addEventListener('click', () => {
-                    const driverDetailsScreen = document.getElementById('driver-details-screen');
-                    const driverConsole = document.getElementById('driver-console');
-                    const searchBar = document.getElementById('search-bar');
-                    driverDetailsScreen.classList.add('hidden');
-                    driverConsole.classList.remove('hidden');
-                    searchBar.value = ''; // Clear the search field when going back to the console
-                });
-            }
+    if (backButton) {
+        backButton.addEventListener('click', () => {
+            window.location.href = './index.html';
         });
+    }
+
+    if (backToConsoleButton) {
+        backToConsoleButton.addEventListener('click', () => {
+            const driverDetailsScreen = document.getElementById('driver-details-screen');
+            const driverConsole = document.getElementById('driver-console');
+            const searchBar = document.getElementById('search-bar');
+            driverDetailsScreen.classList.add('hidden');
+            driverConsole.classList.remove('hidden');
+            searchBar.value = ''; // Clear the search field when going back to the console
+        });
+    }
 });
 
 document.addEventListener('deviceready', function() {
