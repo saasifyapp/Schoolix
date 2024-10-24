3b8efc556a09617740baf8b8bde2ad04b6aa99a2
Yash Ingale <yashingale6@gmail.com>
2024-10-24 12:04:06 +0530

    fix 2

--- App/src/routes/android_app_routes/transport_routes/transport_getStudents.js
+++ App/src/routes/android_app_routes/transport_routes/transport_getStudents.js
@@ -153,12 +153,12 @@ router.get('/android/get-student-details', connectionManagerAndroid, (req, res) 
                     mobile_no AS f_mobile_no, 
                     transport_pickup_drop 
                 FROM teacher_details 
-                WHERE transport_pickup_drop IN (?) AND transport_tagged = ?
+                WHERE transport_pickup_drop IN (?) AND transport_tagged = ? AND (${classesAlloted.map(cls => `FIND_IN_SET(?, classes_alloted) > 0`).join(' OR ')})
             `;
             const studentTeacherValues = [
                 standards, divisions, routeStops, vehicleNo, 
                 standards, divisions, routeStops, vehicleNo, 
-                routeStops, vehicleNo
+                routeStops, vehicleNo, ...classesAlloted
             ];
 
             // Add filtering for route if provided
