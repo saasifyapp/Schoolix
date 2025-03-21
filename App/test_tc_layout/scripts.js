9d20f4b09143efcc3e76c46b418908c5af1f7cac
Yash Ingale <yashingale6@gmail.com>
2025-03-21 09:34:10 +0530

    working on tc

--- App/test_tc_layout/scripts.js
+++ App/test_tc_layout/scripts.js
@@ -1,3 +1,9 @@
 function printContent() {
     window.print();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const table = document.querySelector('.content-table');
+    const rowCount = table.querySelectorAll('tbody tr').length; // Count rows in tbody
+    table.style.setProperty('--row-count', rowCount); // Set CSS variable
+});
